036a81192d6a0bfed9754c85bded02a7827f487c
Wechuli <wechulipaul@yahoo.com>
2018-12-11 11:20:57 +0300

    api redesign

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
+//Require functions
 const express = require("express");
-const personRoute = require("./routes/person");
-const customerRoute = require("./routes/customer");
 const bodyParser = require("body-parser");
+const logger = require("morgan");
 
+//start the express application
 const app = express();
 
-//Use the body parser
-app.use(bodyParser.json());
+//Middleware functions
+app.use(bodyParser.json()); //Use the body parser
+app.use(logger("dev")); //Use morgan to log requests
 
 app.use(express.static("public"));
 
 //Routes
-app.use("/person", personRoute);
-app.use("/customer", customerRoute);
 
-//404 route
-app.use((req, res) => {
-  res.status(404).json({ message: "Resouce Unaivailable" });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "You requested the index page"
+  });
 });
 
+// // Catch 404 errors
+// app.use((req, res) => {
+//   res.status(404).json({ message: "Resouce Unaivailable" });
+// });
+
+//Catch 404 errors and forward them to the error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//Error handler function
+
+app.use((err, req, res, next) => {
+  const error = app.get("env") === "development" ? err : {};
+  const status = err.status || 500;
+  //Respond to client
+  res.status(status).json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+//Start the server on a port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info(`Server has started on port ${PORT}`));
